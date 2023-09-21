2b0fa986081d6760bfeb5d30c23c663d8464a0d0
Mihriban Evren <mihribanevrenn@gmail.com>
2023-09-22 02:14:36 +0300

    Update

---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks'=> Task::Latest()->paginate()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', [
+        'task' => $task
+    ]);
+})->name('tasks.show');
+
+
+Route::post('/tasks', function (TaskRequest $request) {
+    // $data = $request->validated();
+    // $task = new Task;
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+
+    //üsttekileri yazmadan kısaca böyle halledebiliyoruz.
+    $task = Task::create($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created succesfully!');
+})->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    // $data = $request->validated();
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+
+    //üsttekileri yazmadan kısaca böyle halledebiliyoruz.
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated succesfully!');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted succesfully!');
+})->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplate();
+
+    return redirect()->back()->with('success', 'Task updated succesfully!');
+})->name('tasks.toggle-complete');
+
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
